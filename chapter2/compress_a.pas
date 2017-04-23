68adfbba1d78b0af055cce51ef604b14a5e45b01
bewuethr <bewuethr@users.noreply.github.com>
2017-04-23 02:48:02 -0400

    Exercise 2-9: compress longer runs using more characters

---
+++ chapter2/compress_a.pas
@@ -0,0 +1,94 @@
+{ complete compress -- use more printable characters to encode longer runs }
+program compressprog (input, output);
+const
+    ENDFILE = -1;
+    NEWLINE = 10;   { ASCII value }
+    TILDE = 126;
+type
+    character = -1..127;    { ASCII, plus ENDFILE }
+
+{ getc -- get one character from standard input }
+function getc (var c : character) : character;
+var
+    ch : char;
+begin
+    if (eof) then
+        c := ENDFILE
+    else if (eoln) then begin
+        readln;
+        c := NEWLINE
+    end
+    else begin
+        read(ch);
+        c := ord(ch)
+    end;
+    getc := c
+end;
+
+{ putc -- put one character on standard output }
+procedure putc (c : character);
+begin
+    if (c = NEWLINE) then
+        writeln
+    else
+        write(chr(c))
+end;
+
+{ min -- compute minimum of two integers }
+function min(x, y : integer) : integer;
+begin
+    if (x < y) then
+        min := x
+    else
+        min := y
+end;
+
+{ compress -- compress standard input }
+procedure compress;
+const
+    WARNING = TILDE;    { ~ }
+var
+    c, lastc : character;
+    n : integer;
+
+{ putrep -- put out representation of run of n 'c's }
+procedure putrep (N : integer; c : character);
+const
+    MAXREP = 95;    { using all printable ASCII characters }
+    THRESH = 4;
+begin
+    while (n >= THRESH) or ((c = WARNING) and (n > 0)) do begin
+        putc(WARNING);
+        putc(min(n, MAXREP) - 1 + ord(' '));
+        putc(c);
+        n := n - MAXREP
+    end;
+    for n := n downto 1 do
+        putc(c)
+end;
+
+begin
+    n := 1;
+    lastc := getc(lastc);
+    while (lastc <> ENDFILE) do begin
+        if (getc(c) = ENDFILE) then begin
+            if (n > 1) or (lastc = WARNING) then
+                putrep(n, lastc)
+            else
+                putc(lastc)
+        end
+        else if (c = lastc) then
+            n := n + 1
+        else if (n > 1) or (lastc = WARNING) then begin
+            putrep(n, lastc);
+            n := 1
+        end
+        else
+            putc(lastc);
+        lastc := c
+    end
+end;
+
+begin   { main program }
+    compress
+end.
